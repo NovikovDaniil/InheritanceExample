7bea51933179ded4102f6b05d6ead9d35f62e73e
Daniil Novikov <novikovdanya2311@mail.ru>
2020-02-19 21:14:19 +0300

    Completed example1 and task1

---
+++ Task1/Task1.cpp
@@ -0,0 +1,95 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class B1
+{
+	int a;
+public:
+	B1() { cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B1(int x)
+	{
+		a = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~B1() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class B2
+{
+	int b;
+public:
+	B2() { cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B2(int x)
+	{
+		b = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~B2() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class B3
+{
+	int c;
+public:
+	B3() { cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B3(int x)
+	{
+		c = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~B3() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D1 :private B1, public B2, virtual protected B3
+{
+	int d;
+public:
+	D1(int x, int y, int i, int j) :B1(y), B2(i), B3(j)
+	{
+		d = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D1() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D3 :virtual public B3
+{
+	int e;
+public:
+	D3(int x, int y) :B3(y)
+	{
+		e = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D3() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D2 :public D1, private D3
+{
+	int f;
+public:
+	D2(int x, int y, int i, int j, int w, int z, int k) :D1(y, i, j, w), D3(z, k)
+	{
+		f = x;
+
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D2() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+
+class D4 :protected D2
+{
+	int g;
+public:
+	D4(int x, int y, int i, int j, int w, int z, int k, int m) :D2(y, i, j, w, z, k, m)
+	{
+		g = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D4() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+
+};
+
+int main()
+{
+	D3(5, 6);
+	D4(2, 5, 6, 4, 3, 5, 7, 5);
+}
---
+++ Пример 1/Пример 1.cpp
@@ -0,0 +1,59 @@
+﻿// Пример 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+class B {
+	int a;
+public:
+	B() { cout << "object of " << typeid(*this).name() << " was created\n"; }
+	B(int x) {
+		a = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~B() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D1 :virtual public B {
+	int b;
+public:
+
+	D1(int x, int y) :B(y) {
+		b = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D1() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D2 :virtual private B {
+	int c;
+public:
+	D2(int x, int y) :B(y) {
+		c = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D2() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D3 :private D1, public D2 {
+	int d;
+public:
+	D3(int x, int y, int z, int i, int j) :D1(y, z), D2(i, j) {
+		d = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D3() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+class D4 :private D1, public D2 {
+	int e;
+public:
+	D4(int x, int y, int z, int i, int j) :D1(y, z), D2(i, j) {
+		e = x;
+		cout << "object of " << typeid(*this).name() << " was created\n";
+	}
+	~D4() { cout << "object of " << typeid(*this).name() << " was destroyed\n"; }
+};
+
+int main()
+{
+	D3 temp(100, 200, 300, 400, 500);
+	//D4 temp1(1, 2, 3, 4, 5);
+}
+
